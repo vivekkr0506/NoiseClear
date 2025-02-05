fbfc80d0b472f9e26e21ff0378bf96a475177146
Vivek <vivek.kumar02@telusinternational.com>
2025-02-05 12:05:33 +0530

    fixed UI

--- app/src/main/java/com/noiseclear/composable/AudioList.kt
+++ app/src/main/java/com/noiseclear/composable/AudioList.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
@@ -52,7 +53,19 @@ fun AudioList(
     if (filesList.isNotEmpty()) {
         LazyColumn(modifier = Modifier.fillMaxHeight(0.5f).padding(start = 8.dp, end = 8.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
             items(filesList) { file ->
+                Box(
+                    modifier = Modifier
+                        .padding(10.dp) // Outer padding for the border space
+                        .background(
+                            Brush.linearGradient(
+                                colors = listOf(Color.Red, Color.Yellow, Color.Green), // Gradient colors
+                            ),
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                        .padding(2.dp) // Padding between the gradient and the column content
+                ) {
 
+                }
                 Column(modifier = Modifier.border(2.dp, color = Color.Red, shape = RoundedCornerShape(8.dp)).padding(10.dp)){
                     Row(
                         modifier = Modifier
@@ -131,6 +144,7 @@ fun AudioList(
                             LinearProgressIndicator(
                                 progress = { audioProgress },
                                 modifier = Modifier.fillMaxWidth(),
+                                color = Color.Green
                             )
                             Text(
                                 text = String.format(Locale("en"), "%02d:%02d / %02d:%02d", (audioProgress * audioDuration).toInt() / 60, (audioProgress * audioDuration).toInt() % 60, audioDuration.toInt() / 60, audioDuration.toInt() % 60),
